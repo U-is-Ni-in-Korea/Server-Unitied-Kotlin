b697451828f120643ee83d3d3ca8cd1862db8490
jinsu4755 <parkjinsu4755@gmail.com>
2023-09-18 19:57:37 +0900

    feat: Add SnsType

--- module-domain/src/main/kotlin/universe/sparkle/domain/SnsType.kt
+++ module-domain/src/main/kotlin/universe/sparkle/domain/SnsType.kt
@@ -1,5 +1,12 @@
 package universe.sparkle.domain
 
 enum class SnsType {
-    KAKAO, GOOGLE, APPLE
+    KAKAO, GOOGLE, APPLE;
+
+    companion object {
+        fun findSnsTypeBy(snsTypeName: String) : SnsType {
+            return values().find { it.name == snsTypeName }
+                ?: throw IllegalArgumentException("Unsupported social login type: $snsTypeName")
+        }
+    }
 }
