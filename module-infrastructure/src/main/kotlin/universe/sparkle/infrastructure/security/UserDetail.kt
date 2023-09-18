947985647a330e456ce49c6141076b4b851764ba
jinsu4755 <parkjinsu4755@gmail.com>
2023-09-18 21:01:14 +0900

    feat: Add User detail for security

--- module-infrastructure/src/main/kotlin/universe/sparkle/infrastructure/security/UserDetail.kt
+++ module-infrastructure/src/main/kotlin/universe/sparkle/infrastructure/security/UserDetail.kt
@@ -1,37 +1,44 @@
 package universe.sparkle.infrastructure.security
 
 import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.authority.SimpleGrantedAuthority
 import org.springframework.security.core.userdetails.UserDetails
+import universe.sparkle.domain.model.User
+import java.util.stream.Collectors
 
 class UserDetail private constructor(
     private val id: Long,
-    private val nickname:
-): UserDetails {
+    private val nickname: String?,
+    private val snsAuthCode: String,
+    private val authorities: List<GrantedAuthority> = listOf(SimpleGrantedAuthority("User")),
+    val image: String?,
+) : UserDetails {
     override fun getAuthorities(): MutableCollection<out GrantedAuthority> {
-        TODO("Not yet implemented")
+        return authorities.stream().collect(
+            Collectors.toSet(),
+        )
     }
 
-    override fun getPassword(): String {
-        TODO("Not yet implemented")
-    }
+    override fun getPassword(): String = snsAuthCode
 
-    override fun getUsername(): String {
-        TODO("Not yet implemented")
-    }
+    override fun getUsername(): String? = nickname
 
-    override fun isAccountNonExpired(): Boolean {
-        TODO("Not yet implemented")
-    }
+    override fun isAccountNonExpired(): Boolean = true
 
-    override fun isAccountNonLocked(): Boolean {
-        TODO("Not yet implemented")
-    }
+    override fun isAccountNonLocked(): Boolean = true
 
-    override fun isCredentialsNonExpired(): Boolean {
-        TODO("Not yet implemented")
-    }
+    override fun isCredentialsNonExpired(): Boolean = true
 
-    override fun isEnabled(): Boolean {
-        TODO("Not yet implemented")
+    override fun isEnabled(): Boolean = true
+
+    companion object {
+        fun of(user: User): UserDetail {
+            return UserDetail(
+                id = 0,
+                nickname = user.nickname,
+                snsAuthCode = user.snsAuthCode,
+                image = user.image
+            )
+        }
     }
 }
