b468e631d993c83f32c84393e27260961ea40d73
jinsu4755 <parkjinsu4755@gmail.com>
2023-09-20 01:36:30 +0900

    feat: Add jwt value property in JwtConfig

--- module-infrastructure/src/main/kotlin/universe/sparkle/infrastructure/config/JwtConfig.kt
+++ module-infrastructure/src/main/kotlin/universe/sparkle/infrastructure/config/JwtConfig.kt
@@ -1,8 +1,11 @@
 package universe.sparkle.infrastructure.config
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-class JwtConfig {
-
-}
+data class JwtConfig(
+    @Value("\${jwt.secret}") val secret: String,
+    @Value("\${jwt.accessExpiryPeriodDay}") val accessExpiryPeriodDay: Int,
+    @Value("\${jwt.refreshExpiryPeriodDay}") val refreshExpiryPeriodDay: Int,
+)
