dee41a14bdb8174401d73e6791acb9b4e6cd8935
jinsu4755 <parkjinsu4755@gmail.com>
2023-09-20 06:45:12 +0900

    feat: Move ZoneId to JwtConfigContract Companion

--- module-domain/src/main/kotlin/universe/sparkle/domain/JwtConfigContract.kt
+++ module-domain/src/main/kotlin/universe/sparkle/domain/JwtConfigContract.kt
@@ -1,7 +1,13 @@
 package universe.sparkle.domain
 
+import java.time.ZoneId
+
 interface JwtConfigContract {
     fun getSecret(): String
     fun getAccessExpiryPeriodDay(): Long
     fun getRefreshExpiryPeriodDay(): Long
+
+    companion object {
+        val zoneIdKST = ZoneId.of("Asia/Seoul")
+    }
 }
