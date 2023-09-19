de4f90e9c0144e24a1960ee67471dd39ca047efa
jinsu4755 <parkjinsu4755@gmail.com>
2023-09-20 06:46:42 +0900

    test: Add unexpired token verification test

--- module-usecase/src/test/kotlin/universe/sparkle/usecase/BeIssuedAuthTokenUseCaseTest.kt
+++ module-usecase/src/test/kotlin/universe/sparkle/usecase/BeIssuedAuthTokenUseCaseTest.kt
@@ -9,11 +9,13 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertAll
+import org.junit.jupiter.api.assertDoesNotThrow
 import org.junit.jupiter.api.extension.ExtendWith
 import universe.sparkle.domain.JwtConfigContract
 import universe.sparkle.domain.SnsType
 import universe.sparkle.domain.model.User
 import universe.sparkle.usecase.user.BeIssuedAuthTokenUseCase
+import universe.sparkle.usecase.user.ValidateTokenUseCase
 
 @ExtendWith(MockKExtension::class)
 class AuthTokenUseCaseTest {
@@ -24,12 +26,13 @@ class AuthTokenUseCaseTest {
     @InjectMockKs
     private lateinit var beIssuedAuthTokenUseCase: BeIssuedAuthTokenUseCase
 
+    @InjectMockKs
+    private lateinit var validateTokenUseCase: ValidateTokenUseCase
+
     @BeforeEach
     fun setUp() {
-        MockKAnnotations.init(this, relaxed = true, relaxUnitFun = true)
-        every { jwtConfig.getSecret() } answers { "secretSecretSecretSecretSecretSecretSecretSecretSecret" }
-        every { jwtConfig.getAccessExpiryPeriodDay() } answers { 1L }
-        every { jwtConfig.getRefreshExpiryPeriodDay() } answers { 2L }
+        MockKAnnotations.init(this.beIssuedAuthTokenUseCase, relaxed = true, relaxUnitFun = true)
+        MockKAnnotations.init(this.validateTokenUseCase, relaxed = true, relaxUnitFun = true)
     }
 
     @Test
@@ -40,6 +43,9 @@ class AuthTokenUseCaseTest {
             snsType = SnsType.APPLE,
             snsAuthCode = "test",
         )
+        every { jwtConfig.getSecret() } answers { "secretSecretSecretSecretSecretSecretSecretSecretSecret" }
+        every { jwtConfig.getAccessExpiryPeriodDay() } answers { 1L }
+        every { jwtConfig.getRefreshExpiryPeriodDay() } answers { 2L }
         // when
         val authToken = beIssuedAuthTokenUseCase(user)
         // then
@@ -49,4 +55,27 @@ class AuthTokenUseCaseTest {
             { assertThat(authToken.coupleId).isNull() },
         )
     }
+
+    @Test
+    fun 만료되지_않은_유저의_토큰이_주어진_경우_검증을_통과한다() {
+        // given
+        val user = User(
+            id = 1,
+            snsType = SnsType.APPLE,
+            snsAuthCode = "test",
+        )
+        every { jwtConfig.getSecret() } answers { "secretSecretSecretSecretSecretSecretSecretSecretSecret" }
+        every { jwtConfig.getAccessExpiryPeriodDay() } answers { 1L }
+        every { jwtConfig.getRefreshExpiryPeriodDay() } answers { 2L }
+        // when
+        val authToken = beIssuedAuthTokenUseCase(user)
+        // then
+        assertDoesNotThrow {
+            validateTokenUseCase(authToken.accessToken)
+        }
+    }
+
+    // TODO 토큰이 만료된 경우 테스트 작성
+
+    // TODO 토큰이 변조된 경우 테스트 작성
 }
