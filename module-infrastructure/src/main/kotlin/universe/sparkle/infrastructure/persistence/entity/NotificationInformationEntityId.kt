181061a6da14e33eb93ed776d31dde1a90415619
jinsu4755 <parkjinsu4755@gmail.com>
2023-12-13 18:55:06 +0900

    feat: 사용자 알림 정보 테이블 수정 내용 적용

---
+++ module-infrastructure/src/main/kotlin/universe/sparkle/infrastructure/persistence/entity/NotificationInformationEntityId.kt
@@ -0,0 +1,41 @@
+package universe.sparkle.infrastructure.persistence.entity
+
+import jakarta.persistence.Column
+import jakarta.persistence.Embeddable
+import jakarta.validation.constraints.NotNull
+import jakarta.validation.constraints.Size
+import org.hibernate.Hibernate
+import java.io.Serializable
+import java.util.*
+
+@Embeddable
+class NotificationInformationEntityId(
+    userId: Long,
+    fcmToken: String,
+) : Serializable {
+    @NotNull
+    @Column(name = "user_id", nullable = false)
+    var userId: Long = userId
+        protected set
+
+    @Size(max = 255)
+    @NotNull
+    @Column(name = "fcm_token", nullable = false)
+    var fcmToken: String = fcmToken
+        protected set
+
+    override fun hashCode(): Int = Objects.hash(userId, fcmToken)
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other == null || Hibernate.getClass(this) != Hibernate.getClass(other)) return false
+
+        other as NotificationInformationEntityId
+
+        return userId == other.userId &&
+            fcmToken == other.fcmToken
+    }
+
+    companion object {
+        private const val serialVersionUID = -7994741440230625710L
+    }
+}
