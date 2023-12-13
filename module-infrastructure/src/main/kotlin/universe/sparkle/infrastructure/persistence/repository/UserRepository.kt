4bfc10e52b8518b6f3f3c38670f5d5551a8ce076
jinsu4755 <parkjinsu4755@gmail.com>
2023-12-13 19:28:55 +0900

    fix: 사용하지 않는 Dsl 코드 삭제

--- module-infrastructure/src/main/kotlin/universe/sparkle/infrastructure/persistence/repository/UserRepository.kt
+++ module-infrastructure/src/main/kotlin/universe/sparkle/infrastructure/persistence/repository/UserRepository.kt
@@ -3,7 +3,7 @@ package universe.sparkle.infrastructure.persistence.repository
 import org.springframework.data.jpa.repository.JpaRepository
 import universe.sparkle.infrastructure.persistence.entity.UserEntity
 
-interface UserRepository : JpaRepository<UserEntity, Long>, UserDslRepository {
+interface UserRepository : JpaRepository<UserEntity, Long> {
 
     fun findBySnsAuthCode(snsAuthCode: String): UserEntity?
 }
