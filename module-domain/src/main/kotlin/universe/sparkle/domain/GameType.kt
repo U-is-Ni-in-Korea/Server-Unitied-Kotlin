a19e6f88b9ecd830ac5453ec145aa9f1d0738c5b
jinsu4755 <parkjinsu4755@gmail.com>
2023-12-13 18:36:55 +0900

    feat: short game entity 수정으로 인한 변경 사항 적용

--- module-domain/src/main/kotlin/universe/sparkle/domain/GameType.kt
+++ module-domain/src/main/kotlin/universe/sparkle/domain/GameType.kt
@@ -1,5 +1,10 @@
 package universe.sparkle.domain
 
 enum class GameType {
-    SHORT, LONG
+    SHORT, LONG;
+
+    companion object {
+        const val CONTRACT_SHORT = "SHORT"
+        const val CONTRACT_LONG = "LONG"
+    }
 }
